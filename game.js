dbdfa574d2e1a328b642128d4246b3f8d09c389e
Andy Wallace <andymasteroffish@gmail.com>
2020-05-24 18:31:07 -0400

    minor tweak

--- game.js
+++ game.js
@@ -129,6 +129,7 @@ exports.end_game = function(winner){
 
   if (winner != null){
     winner.win_streak++
+    console.log(winner)
   }
 
   pregame_countdown_timer = countdown_ticks_to_game_start
@@ -711,7 +712,6 @@ exports.generate_game_info = function(){
     winner_last_round:prev_winner
   }
 
-  //console.log("game state "+val.game_state)
 
   return val
 }
